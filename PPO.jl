f5a2c9a45a297266907293561525fb27af1ac7e7
ludask1103 <ludask1103@live.se>
2024-07-31 21:52:07 +0200

    Continued testing to fix issues

--- PPO.jl
+++ PPO.jl
@@ -1,4 +1,4 @@
-using Flux, Plots, ReinforcementLearning, Statistics, Random, ProgressBars, Base.Threads, StableRNGs
+using Flux, Plots, ReinforcementLearning, Statistics, Random, ProgressBars, Base.Threads, StableRNGs, ChainRules
 using StatsBase: entropy, wsample
 theme(:ggplot2)
 
@@ -92,7 +92,8 @@ function calculate_advantage_estimate(
 end
 
 function collect_trajectories(
-    workers::Vector{Worker}
+    workers::Vector{Worker},
+    γ::Float64
     )::Trajectory
 
     traj = Trajectory()
@@ -106,6 +107,8 @@ function collect_trajectories(
         append!(traj.state, t.state)
         append!(traj.value, t.value)
     end
+
+    traj.reward = calculate_discounted_rewards(traj.reward,γ)
 
     return traj
 end
@@ -129,8 +132,21 @@ function create_mini_batches(
 
     return batch_ind
 end
-L_e, L_value, L_cl, L = [], [], [], []
-function update_params(
+
+function calculate_discounted_rewards(rewards, γ)
+    T = length(rewards)
+    discounted_rewards = zeros(T)
+    running_add = 0.0
+    for t in T:-1:1
+        running_add = rewards[t] + γ * running_add
+        discounted_rewards[t] = running_add
+    end
+
+    return discounted_rewards
+end
+
+L_e, L_value, L_cl, L, r = [], [], [], [], []
+function _update_params(
     optimizer,
     global_traj::Trajectory,
     global_actor::Chain,
@@ -150,7 +166,7 @@ function update_params(
         for ind in batch_ind
 
             adv = global_traj.advantage[ind]
-            #adv = (adv.-mean(adv))./std(adv)
+            adv = (adv.-mean(adv))./std(adv)
 
             target_value = global_traj.reward[ind]
 
@@ -159,7 +175,7 @@ function update_params(
             prob_old = global_traj.prob[ind]
             prob_old = [prob_old[i][a] for (i,a) in enumerate(actions)]
 
-            ps = Flux.params(global_actor, global_critic)
+            ps = Flux.params([global_actor, global_critic])
             gs = gradient(ps) do
 
                 value = global_critic(states)
@@ -179,36 +195,95 @@ function update_params(
 
                 L_entropy = -sum(exp.(prob) .* prob) * 1 // size(prob, 2)
 
+                ChainRules.ignore_derivatives() do 
+                    push!(L_e,c_2*L_entropy)
+                    push!(L_value,c_1*L_vf)
+                    push!(L_cl,L_clip)
+                    push!(L,-L_clip + c_1*L_vf - c_2*L_entropy)
+                end
+
                 -(L_clip - c_1*L_vf + c_2*L_entropy)
             end
 
-            value = global_critic(states)
-            prob = global_actor(states)
-
-            prob = softmax(prob)
-            
-            prob = log.(prob)
-
-            prob_a = [prob[:,i][a] for (i,a) in enumerate(actions)]
-            
-            L_vf = mean((value .- target_value).^2)
-            ratio = exp.(prob_a .- prob_old)
-
-            surr_1 = ratio .* adv
-            surr_2 = clamp.(ratio,1-ϵ,1+ϵ).*adv
-
-            L_clip = mean(min.(surr_1, surr_2))
-
-            L_entropy = -sum(exp.(prob) .* prob) * 1 // size(prob, 2)
-
             clip_by_global_norm!(gs, ps, 0.5f0)
-            Flux.update!(optimizer, ps, gs)
+            Flux.update!(optimizer, ps, gs)            
+        end
+    end
+end
+function update_params(
+    optimizer_actor,
+    optimizer_critic,
+    global_traj::Trajectory,
+    global_actor::Chain,
+    global_critic::Chain,
+    n_epochs::Int64,
+    batch_size::Int64,
+    c_1::Float64,
+    c_2::Float64,
+    ϵ::Float64
+    )    
 
-            push!(L_e,mean(L_entropy))
-            push!(L_value,mean(L_vf))
-            push!(L_cl,mean(L_clip))
-            push!(L,mean(-L_clip + c_1*L_vf - c_2*L_entropy))
-            
+    NT = length(global_traj.advantage)
+    
+    for k in 1:n_epochs
+        batch_ind = create_mini_batches(NT, batch_size)
+        
+        for ind in batch_ind
+
+            adv = global_traj.advantage[ind]
+            adv = (adv.-mean(adv))./std(adv)
+
+            target_value = global_traj.reward[ind]+adv
+
+            states = transpose(stack(global_traj.state[ind],dims=1))
+            actions = global_traj.action[ind]
+            prob_old = global_traj.prob[ind]
+            prob_old = [prob_old[i][a] for (i,a) in enumerate(actions)]
+
+            ps_actor = Flux.params(global_actor)
+            gs_actor = gradient(ps_actor) do
+                prob = global_actor(states)
+
+                prob = logsoftmax(prob)
+
+                prob_a = [prob[:,i][a] for (i,a) in enumerate(actions)]
+                
+                ratio = exp.(prob_a .- prob_old)
+
+                surr_1 = ratio .* adv
+                surr_2 = clamp.(ratio,1-ϵ,1+ϵ).*adv
+
+                L_clip = mean(min.(surr_1, surr_2))
+
+                L_entropy = -sum(exp.(prob) .* prob) * 1 // size(prob, 2)
+
+                ChainRules.ignore_derivatives() do
+                    append!(r,log.(mean(ratio)))
+                    push!(L_e,-c_2*L_entropy)
+                    push!(L_cl,L_clip)
+                end
+
+                (L_clip - c_2*L_entropy)
+            end
+
+            clip_by_global_norm!(gs_actor, ps_actor, 0.5f0)
+            Flux.update!(optimizer_actor, ps_actor, gs_actor)
+
+            ps_critic = Flux.params(global_critic)
+            gs_critic = gradient(ps_critic) do               
+
+                value = global_critic(states)
+                
+                L_vf = mean((value .- target_value).^2)
+                ChainRules.ignore_derivatives() do 
+                    push!(L_value,c_1*L_vf)
+                end
+
+                c_1*L_vf 
+            end
+
+            clip_by_global_norm!(gs_critic, ps_critic, 0.5f0)
+            Flux.update!(optimizer_critic, ps_critic, gs_critic)            
         end
     end
 end
@@ -232,7 +307,7 @@ function test_actor(
         while !is_terminated(environment)
             s = state(environment)
             
-            probs = softmax(actor(s))
+            probs = logsoftmax(actor(s))
             a = wsample(rng,collect(1:length(probs)),probs)
             
             environment(a)
@@ -245,30 +320,29 @@ end
 
 function main()
 
-    iterations  = 30
+    iterations  = 50
     time_steps  = 2048
     n_envs      = 10
 
     n_epochs    = 10
     batch_size  = 64
     c_1         = 0.5
-    c_2         = 0.01
-    ϵ           = 0.2
+    c_2         = 0.05
+    ϵ           = 0.1
 
     environment = CartPoleEnv(rng=rng)
     num_actions = length(action_space(environment))
     num_states  =  length(state_space(environment))
 
-    actor::Chain = Chain(Dense(num_states => 64,tanh; init=glorot_uniform(rng)), 
-                    Dense(64 => 64,tanh; init=glorot_uniform(rng)),  
-                    Dense(64 => num_actions; init=glorot_uniform(rng))
+    actor::Chain = Chain(Dense(num_states => 32,tanh; init=orthogonal(rng)), 
+                    Dense(32 => num_actions; init=orthogonal(rng))
                     )
 
-    critic::Chain = Chain(Dense(num_states => 64,tanh; init=glorot_uniform(rng)), 
-                    Dense(64 => 64,tanh; init=glorot_uniform(rng)), 
-                    Dense(64 => 1; init=glorot_uniform(rng)))
+    critic::Chain = Chain(Dense(num_states => 32,tanh; init=orthogonal(rng)), 
+                    Dense(32 => 1; init=orthogonal(rng)))
 
-    optimizer = Adam(2.5e-4)
+    optimizer_actor = Adam(3e-4)
+    optimizer_critic = Adam(3e-4)
 
     workers = [Worker(Trajectory(),Agent(actor,critic),CartPoleEnv(rng = StableRNG(hash(1+i))), time_steps) for i in 1:n_envs]
     
@@ -289,9 +363,9 @@ function main()
 
         workers = workers .|> run_policy .|> calculate_advantage_estimate
 
-        traj = collect_trajectories(workers)
+        traj = collect_trajectories(workers,0.99)
 
-        update_params(optimizer,traj,actor,critic,n_epochs,batch_size,c_1,c_2,ϵ)
+        update_params(optimizer_actor,optimizer_critic,traj,actor,critic,n_epochs,batch_size,c_1,c_2,ϵ)
 
         workers = [update_worker(actor,critic,w) for w in workers]
         rew, s, b = test_actor(actor, environment)
@@ -301,13 +375,15 @@ function main()
         push!(span[2],b)
     end
 
-    p1 = plot(range(1,iterations), r, label="Trained agent", title="Rewards"; ribbon=span)
-    plot!(range(1,iterations), r_baseline,label="Baseline"; ribbon=span_baseline)
-
+    #p1 = plot(range(1,iterations), r, label="Trained agent", title="Rewards"; ribbon=span)
+    #plot!(range(1,iterations), r_baseline,label="Baseline"; ribbon=span_baseline)
+    p1 = plot(range(1,iterations), r, label="Trained agent", title="Rewards")
+    plot!(range(1,iterations), r_baseline,label="Baseline")
     p2 = plot(1:length(L_e), L_e, title="Entropy Loss", legend=false)
     p3 = plot(1:length(L_e), L_value, title="Value Loss", legend=false)
     p4 = plot(1:length(L_e), L_cl, title="CLIP Loss", legend=false)
-    p5 = plot(1:length(L_e), L, title="Loss", legend=false)
+    #p5 = plot(1:length(L_e), L, title="Loss", legend=false)
+    p5 = plot(1:length(r), r, title="ratio", legend=false)
     plot(p1,p2,p3,p4,p5,layout=(5,1),size=(700,800))
     
 end
